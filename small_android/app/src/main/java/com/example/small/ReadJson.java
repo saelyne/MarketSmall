f498601be5ac390cfa7643f6911c497d36ddb54e
Jongin Oh <ohjongin@gmail.com>
2017-09-16 13:55:01 +0900

    Build 오류 수정 및 Google Map GMS dependency 추가

--- small_android/app/src/main/java/com/example/small/ReadJson.java
+++ small_android/app/src/main/java/com/example/small/ReadJson.java
@@ -1,11 +1,6 @@
 package com.example.small;
 
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
 /**
  * Created by HONGGIBEOM on 2017. 9. 16..
  */
@@ -13,7 +8,8 @@ import java.util.ArrayList;
 public class ReadJson {
 
     private void readJson() {
-        JSONObject jsonObject = new JSONObject(result); //result를 인자로 넣어 jsonObject를 생성한다.
+        // FIXME:
+        /*JSONObject jsonObject = new JSONObject(result); //result를 인자로 넣어 jsonObject를 생성한다.
 
         JSONArray jsonArray = jsonObject.getJSONArray("dataSet"); //"dataSet"의 jsonObject들을 배열로 저장한다.
 
@@ -23,6 +19,6 @@ public class ReadJson {
         for (int i = 0; i < jsonArray.length(); i++) { //jsonObject에 담긴 두 개의 jsonObject를 jsonArray를 통해 하나씩 호출한다.
             jsonObject = jsonArray.getJSONObject(i);
             list.add(jsonObject.getInt("학번") + " " + jsonObject.getString("이름") + " " + jsonObject.getString("학과"));
-        }
+        }*/
     }
 }
