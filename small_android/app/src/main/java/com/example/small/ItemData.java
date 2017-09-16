a09b96e0afdb880c6412571eb10536c4f5cf18b7
EunChan Park <pec9399@naver.com>
2017-09-17 04:18:31 +0900

    new file

---
+++ small_android/app/src/main/java/com/example/small/ItemData.java
@@ -0,0 +1,36 @@
+package com.example.small;
+
+/**
+ * Created by HONGGIBEOM on 2017. 9. 16..
+ */
+
+public class ItemData {
+
+
+    String phone_number;
+    Long total;
+    String address;
+    String name;
+    Long store_id;
+    Long user_id;
+
+
+
+
+    public ItemData(String phone_number, Long total, String address, String name, Long store_id, Long user_id) {
+        this.phone_number = phone_number;
+        this.total = total;
+        this.address = address;
+        this.name = name;
+        this.store_id=store_id;
+        this.user_id=user_id;
+    }
+
+
+
+
+
+
+
+
+}
