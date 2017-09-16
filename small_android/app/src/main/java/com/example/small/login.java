29ba3ad083629a4fccb61b2defe41afae52a57a8
saelyne <lyne9492@gmail.com>
2017-09-16 16:37:47 +0900

    design applied

--- small_android/app/src/main/java/com/example/small/login.java
+++ small_android/app/src/main/java/com/example/small/login.java
@@ -2,8 +2,12 @@ package com.example.small;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class login extends AppCompatActivity {
@@ -24,5 +28,32 @@ public class login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        showSplash(DateUtils.SECOND_IN_MILLIS);
+    }
+
+    protected void showSplash(long duration)  {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final View layout_splash = findViewById(R.id.layout_splash);
+                Animation anim_fadeout = AnimationUtils.loadAnimation(getBaseContext(), R.anim.fade_out);
+                anim_fadeout.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        layout_splash.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+                layout_splash.startAnimation(anim_fadeout);
+            }
+        }, duration);
     }
 }
